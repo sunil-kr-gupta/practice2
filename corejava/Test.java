7ea8636cad1a302b41177ca0740ab2b5af4858f3
sunil <sunmahika@gmail.com>
2024-11-11 10:21:41 +0530

    second changes made

--- corejava/Test.java
+++ corejava/Test.java
@@ -2,5 +2,7 @@ public class TestJava{
 
 	public static void main(String ...sss){
 		System.out.println("Hello Git...");
+		System.out.println("Hello Git...");
+	System.out.println("Hello Git...");
 	}
 }
